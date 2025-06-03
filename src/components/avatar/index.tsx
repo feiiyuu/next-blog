542e4d29d4776efb84179f55f5a92c2f2da66da2
feiiyuu <wangb.edu@gmail.com>
2025-06-03 17:08:22 +0800

    fix: home

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -1,21 +1,22 @@
 "use client"
 import Svg from "./svg"
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 /* eslint-disable @next/next/no-img-element */
 export const Avatar: React.FC = () => {
     const [isClient, setIsClient] = useState(false)
+    const imgRef = useRef<HTMLImageElement>(null)
     useEffect(() => {
         setIsClient(true)
-    }, [])
+        imgRef.current && (imgRef.current.style.filter = `url(#half-tone-hd)`)
+    })
     if (isClient)
         return (
-
             <div
                 rounded='full'
                 overflow='hidden'
             >
-                 < Svg />
-                <img w='100% md:40vw 2xl:30vw' src="/avtar.jpg" alt="" style={{ filter: 'url(#half-tone-hd)' }} />
+                < Svg />
+                <img ref={imgRef} w='100% md:40vw 2xl:30vw' src="/avtar.jpg" alt="" />
             </div>
         )
     else return <div></div>
