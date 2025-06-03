16bac1f4e6657ebe01cae21b840d1fed2ee44837
feiiyuu <wangb.edu@gmail.com>
2025-06-03 16:05:52 +0800

    fix: home

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -14,8 +14,8 @@ export const Avatar: React.FC = () => {
                 rounded='full'
                 overflow='hidden'
             >
+                 < Svg />
                 <img w='100% md:40vw 2xl:30vw' src="/avtar.jpg" alt="" style={{ filter: 'url(#half-tone-hd)' }} />
-                < Svg />
             </div>
         )
     else return <div></div>
